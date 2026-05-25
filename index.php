2048a73ed735399cb73991e3a8fd540938ca7fe5
Tulio Orione <tulioorione@gmail.com>
2026-05-25 09:59:40 -0300

    Adiciona tabela para exibir lista de contatos

--- index.php
+++ index.php
@@ -14,7 +14,30 @@ include_once("templates/header.php");
     <?php endif; ?>
     <h1 id="main-title">Minha Agenda</h1>
     <?php if(count($contacts) > 0): ?>
-        <p>TEM CONTATOS</p>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row"><?= $contact["id"] ?></td>
+                        <td scope="row"><?= $contact["name"] ?></td>
+                        <td scope="row"><?= $contact["phone"] ?></td>
+                        <td class="actions">
+                            <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="#"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>    
+            </tbody>
+        </table>
     <?php else: ?>
         <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
     <?php endif; ?>
