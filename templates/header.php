158dc076e91e002b8fe61f27af705d811e58a388
Tulio Orione <tulioorione@gmail.com>
2026-05-25 10:23:34 -0300

    Adiciona estilos visuais à tabela de contatos e ao layout

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,7 @@
 <?php
 include_once("config/url.php");
+
+$cssVersion = filemtime(__DIR__ . "/../css/styles.css");
 ?>
 
 
@@ -14,7 +16,7 @@ include_once("config/url.php");
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css?v=<?= $cssVersion ?>">
 </head>
 <body>
     <header>
