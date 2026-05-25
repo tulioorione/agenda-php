db3d496b2fcefd5e467ead0c4e0a9f7e21ca4c10
Tulio Orione <tulioorione@gmail.com>
2026-05-25 11:10:17 -0300

    Adiciona formulário de criação de contato com validação

--- create.php
+++ create.php
@@ -1,8 +1,28 @@
 <?php
 include_once("templates/header.php");
+include_once("config/url.php");
 ?>
-    
-<h1>Criando contato</h1>
+
+<div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title">Criar contato</h1>
+    <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <input type="hidden" name="type" value="create">
+        <div class="form-group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+        </div>
+        <div class="form-group">
+            <label for="observations">Observação:</label>
+            <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Digite as observações" rows="3"></textarea> 
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
 
 <?php
 include_once("templates/footer.php");
